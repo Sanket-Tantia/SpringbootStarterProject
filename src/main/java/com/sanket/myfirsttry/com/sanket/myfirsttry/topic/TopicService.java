34b5d9511746397e7c2f0911f0b1b9bced60b6a1
Sanket Tantia <19sanket95@gmail.com>
2018-02-23 00:01:00 +0530

    implemented crud repository

--- src/main/java/com/sanket/myfirsttry/com/sanket/myfirsttry/topic/TopicService.java
+++ src/main/java/com/sanket/myfirsttry/com/sanket/myfirsttry/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.sanket.myfirsttry.com.sanket.myfirsttry.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,37 +10,47 @@ import java.util.List;
 @Service
 public class TopicService {
 
-    private List<TopicData> alltopics = new ArrayList<>( Arrays.asList(
-        new TopicData("abc","course","des"),
-                new TopicData("xyz","course1","des1")
-        ));
+    @Autowired
+    private TopicRepository topicRepository;
+
+//    private List<TopicData> alltopics = new ArrayList<>( Arrays.asList(
+//        new TopicData("abc","course","des"),
+//                new TopicData("xyz","course1","des1")
+//        ));
 
     public List<TopicData> getAllTopics(){
+        //return alltopics;
+        List<TopicData> alltopics = new ArrayList<>();
+        topicRepository.findAll().forEach(alltopics::add);
         return alltopics;
     }
 
     public TopicData getTopic(String id){
-        return alltopics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
+        //return alltopics.stream().filter(t ->t.getId().equals(id)).findFirst().get();
+        return topicRepository.findOne(id);
     }
 
     public void addTopic(TopicData newTopic) {
-        alltopics.add(newTopic);
+        //alltopics.add(newTopic);
+        topicRepository.save(newTopic);
     }
 
-    public String updateTopic(String id, TopicData updatedTopic) {
+    public void updateTopic(String id, TopicData updatedTopic) {
         //alltopics.stream().filter(t ->t.getId().equals(id)).findFirst().set
 
-        for(int i=0;i<alltopics.size();i++){
-            TopicData t = alltopics.get(i);
-            if(t.getId()==id){
-                alltopics.set(i,updatedTopic);
-                return "true";
-            }
-        }
-        return id;
+//        for(int i=0;i<alltopics.size();i++){
+//            TopicData t = alltopics.get(i);
+//            if(t.getId()==id){
+//                alltopics.set(i,updatedTopic);
+//                return "true";
+//            }
+//        }
+//        return id;
+        topicRepository.save(updatedTopic);
     }
 
     public void deleteTopic(String id) {
-        alltopics.removeIf(t -> t.getId().equals(id));
+        //alltopics.removeIf(t -> t.getId().equals(id));
+        topicRepository.delete(id);
     }
 }
